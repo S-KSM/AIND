fc0bec08102e67b06ca33eeb97ce91ca2e81bc9f
Shobeir <shobeir.mazinani@gmail.com>
2017-02-26 19:38:30 -0700

    commit

--- sudoku/function.py
+++ sudoku/function.py
@@ -29,4 +29,22 @@ def grid_values(grid):
     return sdkdict
 
 
+
+def eliminate(values):
+    """Eliminate values from peers of each box with a single value.
+
+    Go through all the boxes, and whenever there is a box with a single value,
+    eliminate this value from the set of values of all its peers.
+
+    Args:
+        values: Sudoku in dictionary form.
+    Returns:
+        Resulting Sudoku in dictionary form after eliminating values.
+    """
+
+    for i ,j in boxes
+    return values
+
+
+
 display(grid_values('..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..'))
